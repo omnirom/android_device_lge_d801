ba12385a278638067fdf1b823674f97d7eab385e
Jake Whatley <jacob.whatley91@gmail.com>
2014-10-12 14:20:00 -0400

    update d801
    Change-Id: I32bca35bf46852284a67f62b17e4e5e5be839fb7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@
 
 include device/lge/g2-common/BoardConfigCommon.mk
 
-TARGET_KERNEL_CONFIG := custom_d801_defconfig
+TARGET_KERNEL_CONFIG := msm8974_d801_defconfig
 
 TARGET_OTA_ASSERT_DEVICE := d801,g2,galbi
 
